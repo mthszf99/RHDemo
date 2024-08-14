3ceb6aa607ff7e06cfa4ce1dfcb6b480f11dad42
mthszf99 <matheusf99@outlook.com>
2024-08-14 14:29:56 -0300

    commit initial

---
+++ RHDemo/src/main/java/com/RHDemo/RHDemo/RhDemoApplication.java
@@ -0,0 +1,13 @@
+package com.RHDemo.RHDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhDemoApplication.class, args);
+	}
+
+}
---
+++ RHDemo/src/test/java/com/RHDemo/RHDemo/RhDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RHDemo.RHDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
