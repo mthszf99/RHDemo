71665f4845f34cfa60cfdf51a2f23ed06443ac16
mthszf99 <matheusf99@outlook.com>
2024-08-26 15:38:22 -0300

    fix 0002

--- RHPro/src/main/java/com/RHPro/RHPro/controllers/VagaController.java
+++ RHPro/src/main/java/com/RHPro/RHPro/controllers/VagaController.java
@@ -6,6 +6,7 @@ import com.RHPro.RHPro.models.Vaga;
 import com.RHPro.RHPro.repository.CandidatoRepository;
 import com.RHPro.RHPro.repository.VagaRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +18,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class VagaController {
 
-    @AutoWired
+    @Autowired
     private VagaRepository vr;
-    @AutoWired
+    @Autowired
     private CandidatoRepository cr;
 
     //GET CADASTRAR A VAGA
